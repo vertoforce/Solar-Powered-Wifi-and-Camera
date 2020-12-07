68a21fe5704858a16698fb2b95e423292ebe338a
Connor Lake <vertoforce@gmail.com>
2020-12-07 12:03:19 -0500

    Added GetAttributes function

--- src/upload.cpp
+++ src/upload.cpp
@@ -1,6 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClientSecure.h>
+#include <ArduinoJson.h>
 
 #include "upload_creds.h"
 
@@ -8,14 +9,12 @@
 WiFiClientSecure client;
 
 // Post makes a post to thingsboard with the provided path and body.
-int Post(String path, String body) {
+String Post(String method, String path, String body) {
     if (!client.connect(UPLOAD_TCP_SERVER, UPLOAD_TCP_PORT)) {
         Serial.println("failed to connect");
-        return -1;
+        return "error";
     }
-    client.print("POST ");
-    client.print(path);
-    client.println(" HTTP/1.1");
+    client.println(method+" "+path+" HTTP/1.1");
     client.println("Connection: close");
     client.println("User-Agent: device");
     client.println("Content-Type: application/json");
@@ -28,19 +27,47 @@ int Post(String path, String body) {
     client.print(body.c_str());
     client.println();
     client.flush();
+
+    // TODO: untested, This will probably return the full body including the headers
+    String body = client.readString();
     client.stop();
 
-    // TODO: return response
-
-    return 0;
+    return body;
 }
 
 // SendAttributes sends thingsboard attributes
-int SendAttributes(String attributes) {
-    return Post("/api/v1/" ACCESS_TOKEN "/attributes", attributes);
+String SendAttributes(String attributes) {
+    return Post("POST", "/api/v1/" ACCESS_TOKEN "/attributes", attributes);
 }
 
 // SendTelemetry Sends thingsboard telemetry data
-int SendTelemetry(String data) {
-    return Post("/api/v1/" ACCESS_TOKEN "/telemetry", data);
+String SendTelemetry(String data) {
+    return Post("POST", "/api/v1/" ACCESS_TOKEN "/telemetry", data);
+}
+
+// Get Attributes returns the attributes and their values from the server
+StaticJsonDocument<400> GetAttributes(String clientKeys[], String sharedKeys[]) {
+    String clientKeysString = "";
+    for (int i=0;i<sizeof(clientKeys);i++) {
+        clientKeysString += clientKeys[i];
+        if (i != sizeof(clientKeys)) {
+            clientKeysString += ",";
+        }
+    }
+
+    String sharedKeysString = "";
+    for (int i=0;i<sizeof(clientKeys);i++) {
+        clientKeysString += clientKeys[i];
+        if (i != sizeof(clientKeys)) {
+            clientKeysString += ",";
+        }
+    }
+
+ 
+    String body = Post("GET", "/api/v1/" ACCESS_TOKEN "/attributes?clientKeys="+clientKeysString+"&sharedKeys="+sharedKeysString, "");
+
+    StaticJsonDocument<400> doc;
+    deserializeJson(doc, body);
+
+    return doc;
 }
