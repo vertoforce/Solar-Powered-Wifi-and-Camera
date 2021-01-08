61e65c79ba4168c6e692d98bd9665d83efe3443e
Connor Lake <vertoforce@gmail.com>
2021-01-08 11:53:02 -0800

    Fixed upload helper

--- src/upload.cpp
+++ src/upload.cpp
@@ -11,28 +11,27 @@ WiFiClientSecure client;
 
 // Post makes a post request to the provided path with the provided body.
 String Post(String method, String path, String body) {
-    if (ESP.getFreeHeap() < 28000) {
-        // Do not perform anything, low memory
-        return "not enough heap";
+    client.setInsecure();
+    if (!client.connect(UPLOAD_TCP_SERVER, UPLOAD_TCP_PORT)) {
+        Serial.println("failed to connect");
+        return "error";
     }
+    client.println(method+" "+path+" HTTP/1.1");
+    client.println("Connection: close");
+    client.println("User-Agent: device");
+    client.println("Content-Type: application/json");
+    client.print("Host: ");
+    client.println(UPLOAD_TCP_SERVER);
+    client.print("Content-Length: ");
+    client.print(body.length());
+    client.println();
+    client.println();
+    client.print(body.c_str());
+    client.println();
+    client.flush();
 
-    fetch.begin(UPLOAD_TCP_SERVER + path, true);
-    // fetch.addHeader("User-Agent", "kilnmon-device");
-    if (method == "GET") {
-        fetch.GET(path);
-    } else if (method == "POST") {
-        fetch.addHeader("Content-Type", "application/json");
-        fetch.POST(body);
-    }
-
-    // Read body
-    String returnBody = "";
-    while (fetch.busy()) {
-        if (fetch.available()) {
-           returnBody += fetch.read();
-        }
-    }
-    fetch.clean();
+    String returnBody = client.readString();
+    client.stop();
 
     return returnBody;
 }
@@ -50,28 +49,14 @@ String SendTelemetry(String data) {
 }
 
 // Get Attributes returns the attributes and their values from the server
-StaticJsonDocument<400> GetAttributes(String clientKeys[],
-                                      String sharedKeys[]) {
-    String clientKeysString = "";
-    for (int i = 0; i < sizeof(clientKeys); i++) {
-        clientKeysString += clientKeys[i];
-        if (i != sizeof(clientKeys)) {
-            clientKeysString += ",";
-        }
-    }
-
-    String sharedKeysString = "";
-    for (int i = 0; i < sizeof(clientKeys); i++) {
-        clientKeysString += clientKeys[i];
-        if (i != sizeof(clientKeys)) {
-            clientKeysString += ",";
-        }
-    }
-
-    String body = Post("GET",
-                       "/api/v1/" ACCESS_TOKEN "/attributes?clientKeys=" +
-                           clientKeysString + "&sharedKeys=" + sharedKeysString,
-                       "");
+// clientKeys and sharedKeys are a comma separated stirng of requested keys.
+StaticJsonDocument<400> GetAttributes(char *clientKeys, char* sharedKeys) {
+    char url[150];
+    sprintf(url, "/api/v1/" ACCESS_TOKEN "/attributes?clientKeys=%s&sharedKeys=%s", clientKeys, sharedKeys);
+    Serial.println("url");
+    Serial.println(url);
+    String body = Post("GET", String(url), "");
+    Serial.println(body);
 
     StaticJsonDocument<400> doc;
     deserializeJson(doc, body);
--- src/upload.h
+++ src/upload.h
@@ -1,1 +1,5 @@
-int SendAttributes(String attributes);
+#include <ArduinoJson.h>
+
+String SendAttributes(String attributes);
+String SendTelemetry(String data);
+StaticJsonDocument<400> GetAttributes(char* clientKeys, char* sharedKeys);
